defce045fab988a0894e0848031561e8fa545705
berktepebag <berktepebag@gmail.com>
2019-03-10 17:06:52 +0100

    Update Mobile Base Control & Arm Control

--- src/robot_controller.cpp
+++ src/robot_controller.cpp
@@ -2,6 +2,7 @@
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
 #include <std_msgs/Float64MultiArray.h>
+#include <std_msgs/Float64.h>
 #include <sensor_msgs/JointState.h>
 #include <nav_msgs/Odometry.h>
 #include <rosgraph_msgs/Clock.h>
@@ -31,6 +32,10 @@ private:
   double pos_link0,pos_link1,pos_link2,pos_link3;
   double link0_mult,link1_mult,link2_mult,link3_mult;
   double arm0,arm1,arm2,arm3;
+  std_msgs::Float64 arm0_msg,arm1_msg,arm2_msg,arm3_msg;
+  double linear_axe,angular_axe;
+
+  int current_joint;
 
   //Timing
   double ros_clock_sec;
@@ -47,6 +52,12 @@ private:
   
   ros::Publisher vel_pub_;
   ros::Publisher arm_pub_;
+
+  ros::Publisher arm0_pub_;
+  ros::Publisher arm1_pub_;
+  ros::Publisher arm2_pub_;
+  ros::Publisher arm3_pub_;
+
   ros::Subscriber joy_sub_;
   ros::Subscriber joint_states_sub;
   ros::Subscriber odom_sub_;
@@ -63,6 +74,7 @@ linear_arm(4)
 {
 
   linear_speed_goal=0;
+  current_joint = -1;
 
   nh_.param("axis_linear", linear_, linear_);
   nh_.param("arm_axis_linear", linear_arm, linear_arm);
@@ -74,10 +86,13 @@ linear_arm(4)
   nh_.param("scale_linear", l_scale_, l_scale_);
 
 
-  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/mobile_base_controller/cmd_vel", 1);
+  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/mobile_base_controller/cmd_vel", 10);
 
-  arm_pub_ = nh_.advertise<std_msgs::Float64MultiArray>("/mra_armLinks_controller/command", 1);
-  
+  //arm_pub_ = nh_.advertise<std_msgs::Float64MultiArray>("/mra_armLinks_controller/command", 1);
+  arm0_pub_ = nh_.advertise<std_msgs::Float64>("/joint0_position_controller/command", 10);
+  arm1_pub_ = nh_.advertise<std_msgs::Float64>("/joint1_position_controller/command", 10);
+  arm2_pub_ = nh_.advertise<std_msgs::Float64>("/joint2_position_controller/command", 10);
+  arm3_pub_ = nh_.advertise<std_msgs::Float64>("/joint3_position_controller/command", 10);
 
   joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopInnomechRobot::joyCallback, this);
   odom_sub_ = nh_.subscribe<nav_msgs::Odometry>("/mobile_base_controller/odom",10, &TeleopInnomechRobot::odomCallback, this);
@@ -102,25 +117,29 @@ double TeleopInnomechRobot::pidContoller(){
   double current_time = ros_clock_sec;
   double dt = current_time - previous_time;
 
-  double Kp = 10;
-  double Kd = 1;
+  double Kp = 100;
+  double Kd = 10;
   double linear_error = (linear_speed_goal-linear_odom_x);
 
+  /*
   cout << "***---------***" <<endl;
   cout << "linear_speed_goal: " << linear_speed_goal << endl;
   cout << "angular_speed_goal: " << angular_speed_goal << endl;
   cout << "linear_odom_x: " << linear_odom_x << endl;
   cout << "linear_error: " << linear_error << endl;
+  */
+  double linear_command = 0;
 
-  double de = previous_linear_error- linear_error;
-  double linear_command = Kp * linear_error + Kd * (de/dt);
-
-  previous_linear_error = linear_error; 
-  
+  if (abs(linear_error)>0.01 )
+  {
+    double de = previous_linear_error- linear_error;
+    linear_command = Kp * linear_error + Kd * (de/dt);
+    previous_linear_error = linear_error; 
   //cout << "time difference: "<< dt << endl;
-  previous_time = ros_clock_sec;
+    previous_time = ros_clock_sec;
+  //cout << "linear_command: " <<linear_command << endl;
 
-  cout << "linear_command: " <<linear_command << endl;
+  }
 
   return linear_command;
 }
@@ -137,43 +156,69 @@ void TeleopInnomechRobot::jointStateCallback(const sensor_msgs::JointState::Cons
 void TeleopInnomechRobot::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
   //Arm Movement
-  double link_mult[]={1.0,0.95,0.90,0.85};
+  double link_mult[]={0.5,0.25,0.25,0.25};  
+
+  linear_axe = joy->axes[linear_arm];
+  angular_axe = joy->axes[angular_arm];
+
+  if (joy->buttons[4] && current_joint > 0)
+  {
+    current_joint--;
+    cout << "Current Joint Changed: " << current_joint <<endl;
+  }
+  else if (joy->buttons[5] & current_joint < 3)
+  {
+    current_joint++;
+    cout << "Current Joint Changed: " << current_joint <<endl;
+  }
+
+
   
   if (joy->axes[angular_arm] || joy->axes[linear_arm])
-  {
-    arm0 = pos_link0+joy->axes[angular_arm]*link_mult[0];
-    arm1 = pos_link1+joy->axes[linear_arm] *link_mult[1];
-    arm2 = pos_link2+joy->axes[linear_arm] *link_mult[2];
-    arm3 = pos_link3+joy->axes[linear_arm] *link_mult[3];
-
-    if (joy->buttons[10])
+  { 
+    cout << "Current Joint: " << current_joint <<endl;
+    if (current_joint==0)
     {
+      arm0 = pos_link0 + angular_axe*link_mult[0];
+      /*cout << "joy->axes[angular_arm]*link_mult[0]: " << angular_axe*link_mult[0] <<endl;
+      cout << "pos_link0: " << pos_link0 << endl;
+      cout << "arm0: " << arm0_msg.data << endl;*/
+    }
+    else if (current_joint==1){
+      arm1 = pos_link1 +joy->axes[linear_arm] *link_mult[1];
+      /*cout << "joy->axes[linear_arm]*link_mult[1]: " << joy->axes[linear_arm]*link_mult[1] <<endl;
+      cout << "pos_link1: " << pos_link1 << endl;
+      cout << "arm1: " << arm1_msg.data << endl;*/
+    }
+    else if (current_joint==2){    
+      arm2 = pos_link2 + linear_axe *link_mult[2];
+      /*cout << "linear_axe*link_mult[2]: " << linear_axe*link_mult[2] <<endl;
+      cout << "pos_link2: " << pos_link2 << endl;
+      cout << "arm2: " << arm2_msg.data << endl;*/
+    }
+    else if (current_joint==3){
+      arm3 = pos_link3 + linear_axe *link_mult[3];
+      /*cout << "linear_axe*link_mult[3]: " << linear_axe*link_mult[3] <<endl;
+      cout << "pos_link3: " << pos_link3 << endl;
+      cout << "arm3: " << arm3_msg.data << endl;*/
+    }
+
+
+    if (joy->buttons[10]){
      arm0 = 0;
      arm1 = 0;
      arm2 = 0;
-     arm3 = 0;
+     arm3 = 0; 
    }
-  //cout << "arm0: " << arm0 << endl;
-   std::vector<double> arm_vector;
-   arm_vector.push_back(arm0);
-   arm_vector.push_back(arm1);
-   arm_vector.push_back(arm2);
-   arm_vector.push_back(arm3);
+
+   arm0_msg.data = arm0;
+   arm1_msg.data = arm1;
+   arm2_msg.data = arm2;
+   arm3_msg.data = arm3;
 
 
-   std_msgs::Float64MultiArray arm_msg;
-
-   arm_msg.layout.dim.push_back(std_msgs::MultiArrayDimension());
-   arm_msg.layout.dim[0].size = 4;
-   arm_msg.layout.dim[0].stride = 1;
-   arm_msg.layout.dim[0].label =' ';
-
-
-   arm_msg.data.clear();
-   arm_msg.data.insert(arm_msg.data.end(), arm_vector.begin(), arm_vector.end());
-
-   arm_pub_.publish(arm_msg);
  }
+
  //Mobile Base Movement
 
  min_linear_speed= -0.2;
@@ -201,17 +246,14 @@ angular_speed_goal = joy->axes[angular_];
 }
 
 void TeleopInnomechRobot::publishSpeed(){
-  /*
-  cout << "-----------------------------"<<endl;
-  cout << "current linear.x: " << twist.linear.x << endl;
-  cout << "current angular.y: " << twist.angular.y << endl;
-  */
-  //twist.linear.x = joy->axes[linear_];
-  //twist.linear.x = linear_speed_goal;
   twist.linear.x = pidContoller();
   twist.angular.z = angular_speed_goal;
   vel_pub_.publish(twist);
 
+  arm0_pub_.publish(arm0_msg);
+  arm1_pub_.publish(arm1_msg);
+  arm2_pub_.publish(arm2_msg);
+  arm3_pub_.publish(arm3_msg);
 }
 
 
